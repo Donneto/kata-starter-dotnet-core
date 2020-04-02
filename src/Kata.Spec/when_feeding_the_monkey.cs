98eb28733db7ac713594c153ff50edcf57af7253
Nestor Escoto <nestor.escoto15@gmail.com>
2020-04-02 14:47:35 -0600

    Adding multiple numbers

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using FluentAssertions;
 using Machine.Specifications;
 
@@ -50,6 +51,18 @@ namespace Kata.Spec
         private static int _result;
         private static Calculator _systemUnderTest;
     }
+    
+    public class when_user_input_multiple_numbers
+    {
+        private Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
+
+        It should_return_the_sum_of_numbers= () => { _result.Should().Be(6); };
+        private static int _result;
+        private static Calculator _systemUnderTest;
+    }
+
 
 
     internal class Calculator
@@ -61,13 +74,19 @@ namespace Kata.Spec
             {
                 return 0;
             }
-            
+
+            var number = num.Split(",");
+
+            if (number.Length > 1)
+            {
+                return int.Parse(number[0]) + int.Parse(number[1]);
+            }
+
             return Int32.Parse(num);
         }
     }
 
     
-    // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
     // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
     // Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
     // Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
